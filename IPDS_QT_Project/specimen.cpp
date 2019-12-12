2a0983ba77d71f983cc9599ab750dc1c75f619a7
Vladimir <vladimir@wings.rs>
2019-12-12 19:13:22 +0100

    Red Hot Chicks update
    Added chicks to qrc, replaced all primitive shapes with purple chicks

--- IPDS_QT_Project/specimen.cpp
+++ IPDS_QT_Project/specimen.cpp
@@ -42,21 +42,27 @@ void Specimen::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, 
     painter->drawRoundedRect(0, 0, 10, 10, 5, 5);
     double angle = this->specimenID * ( (2*M_PI) / ID );
     std::cout << "ID:" << ID << "\n" << "staticCount:" << this->specimenID <<"\n" <<std::endl;
-    double r = 140;
+    double r = 160;
     double tmpX = r * std::cos(angle);
     double tmpY = r * std::sin(angle);
 
 
-    painter->setBrush(color);
-    painter->drawEllipse(tmpX,tmpY, 10, 10);
+//    painter->setBrush(color);
+//    painter->drawEllipse(tmpX,tmpY, 10, 10);
 
-    painter->drawImage(QRect(-10, -20, 20, 40), QImage(QString::fromStdString(imgPath)));
+    QPoint point(tmpX,tmpY);
+    QImage image("://chickPics/purple.png");
+
+    painter->drawImage(point, image);
+
+//    painter->drawImage(QRect(-10, -20, 20, 40), QImage(QString::fromStdString(":/chickPics/chickPics/blue.png")));
 
 }
 
 void Specimen::advance(int step)
 {
     Q_UNUSED(step)
+
     return;
 }
 
