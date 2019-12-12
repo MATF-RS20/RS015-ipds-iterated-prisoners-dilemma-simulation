b4896a426cb957548bce40e5f150f46bd00a0268
Vladimir <vladimir@wings.rs>
2019-12-12 01:26:42 +0100

    Added food
    Added food class for easier handling of specimen encounters

--- IPDS_QT_Project/food.cpp
+++ IPDS_QT_Project/food.cpp
@@ -1,6 +1,37 @@
 #include "food.h"
 
-Food::Food()
+Food::Food(double posX, double posY)
+    :m_posX(posX)
+    ,m_posY(posY)
 {
+    this->specimen1=nullptr;
+    this->specimen2=nullptr;
+    this->m_noOfSpecimen=0;
+}
 
+bool Food::addSpecimen(Specimen* newSpecimen)
+{
+    if(this->specimen1 == nullptr)
+    {
+        this->specimen1 = newSpecimen;
+        return true;
+    }
+    else if(this->specimen2 == nullptr)
+    {
+        this->specimen2 = newSpecimen;
+        return true;
+    }
+    return false;
+
+}
+void Food::releaseSpecimen()
+{
+    this->specimen1=nullptr;
+    this->specimen2=nullptr;
+
+}
+
+int Food::numPresent()
+{
+    return this->m_noOfSpecimen;
 }
