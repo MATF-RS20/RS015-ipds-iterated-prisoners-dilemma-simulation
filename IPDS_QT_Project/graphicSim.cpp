7bca4a2089d911284b44fbb8b9f5edeb6f3f00a2
vbatocanin <vladimir@wings.rs>
2019-12-16 12:34:53 +0100

    Reformatted code

--- IPDS_QT_Project/graphicSim.cpp
+++ IPDS_QT_Project/graphicSim.cpp
@@ -1,112 +1,86 @@
 #define _USE_MATH_DEFINES
 
-
 #include "graphicSim.h"
 
 static const double PI = 3.14159265358979323846264338327950288419717;
 static const double TWO_PI = 2.0 * PI;
 
 /*function that maps given angle to [0,2*PI]*/
-static qreal normalizeAngle(qreal angle)
-{
-    while (angle < 0)
-    {
-        angle += TWO_PI;
-    }
+static qreal normalizeAngle(qreal angle) {
+  while (angle < 0) {
+    angle += TWO_PI;
+  }
 
-    while (angle > TWO_PI)
-    {
-        angle -= TWO_PI;
-    }
+  while (angle > TWO_PI) {
+    angle -= TWO_PI;
+  }
 
-    return angle;
+  return angle;
 }
 
 GraphicSim::GraphicSim(unsigned foodNo, std::vector<unsigned> &specimenNoInfo)
-    : Simulation(foodNo,specimenNoInfo)
-{
+    : Simulation(foodNo, specimenNoInfo) {
 
-    /*Initializes Simulation and by extension, all specimen instances*/
-    unsigned strategyCount = strategy::COUNT;
+  /*Initializes Simulation and by extension, all specimen instances*/
+  unsigned strategyCount = strategy::COUNT;
 
+  for (unsigned i = 0; i < strategyCount; ++i) {
+    unsigned long curSpecimenNo = this->m_specimen[i].size();
 
-
-    for(unsigned i=0; i<strategyCount; ++i)
-    {
-        unsigned long curSpecimenNo = this->m_specimen[i].size();
-
-        for(unsigned long j=0; j<curSpecimenNo ; ++j)
-        {
-            this->m_specimen[i][j]->show();
-        }
-
+    for (unsigned long j = 0; j < curSpecimenNo; ++j) {
+      this->m_specimen[i][j]->show();
     }
+  }
 
-    this->initializeFood(foodNo);
-
-
+  this->initializeFood(foodNo);
 }
-GraphicSim::~GraphicSim(){
+GraphicSim::~GraphicSim() {}
+void GraphicSim::show() {
+  unsigned strategyCount = strategy::COUNT;
+  for (unsigned i = 0; i < strategyCount; ++i) {
+    unsigned long curSpecimenNo = this->m_specimen[i].size();
 
-}
-void GraphicSim::show()
-{
-    unsigned strategyCount = strategy::COUNT;
-    for(unsigned i=0; i<strategyCount; ++i)
-    {
-        unsigned long curSpecimenNo = this->m_specimen[i].size();
-
-        for(unsigned long j=0; j<curSpecimenNo ; ++j)
-        {
-            this->m_specimen[i][j]->show();
-        }
-
+    for (unsigned long j = 0; j < curSpecimenNo; ++j) {
+      this->m_specimen[i][j]->show();
     }
+  }
 }
 
-void GraphicSim::addItems(QGraphicsScene& scene)
-{
-    for(auto stratVector : this->m_specimen)
-    {
-        for(auto tmpSpecimen : stratVector)
-        {
-            scene.addItem(tmpSpecimen.get());
-        }
+void GraphicSim::addItems(QGraphicsScene &scene) {
+  for (auto stratVector : this->m_specimen) {
+    for (auto tmpSpecimen : stratVector) {
+      scene.addItem(tmpSpecimen.get());
     }
+  }
 }
 
-void GraphicSim::initializeFood(int foodNo)
-{
-    //TODO add global drawing center,scale to R
-    double randa,randr;
-    double centerX = -10.0;
-    double centerY = -20.0;
+void GraphicSim::initializeFood(int foodNo) {
+  // TODO add global drawing center,scale to R
+  double randa, randr;
+  double centerX = -10.0;
+  double centerY = -20.0;
 
-    /*Generates random position inside a circle*/
-    /*randr signifies the distance from the center, randa signifies the angle for the polar coordinates*/
-    for(int i=0;i<foodNo; ++i)
-    {
-        //TODO clean up this blasphemous random number generation and pi representation
-        //TODO tweak size of r to match window size
+  /*Generates random position inside a circle*/
+  /*randr signifies the distance from the center, randa signifies the angle for
+   * the polar coordinates*/
+  for (int i = 0; i < foodNo; ++i) {
+    // TODO clean up this blasphemous random number generation and pi
+    // representation
+    // TODO tweak size of r to match window size
 
+    randr = static_cast<double>(qrand()) / RAND_MAX;
+    double r = randr * sqrt(centerX * centerX + centerY * centerY);
 
-        randr = static_cast<double>(qrand())/RAND_MAX;
-        double r = randr*sqrt(centerX*centerX+centerY*centerY);
+    randa = static_cast<double>(qrand()) / RAND_MAX;
+    double tmpX = r * std::cos(randa * 2 * M_PI);
+    double tmpY = r * std::sin(randa * 2 * M_PI);
 
-
-        randa = static_cast<double>(qrand())/RAND_MAX;
-        double tmpX = r * std::cos(randa*2*M_PI);
-        double tmpY = r * std::sin(randa*2*M_PI);
-
-        /*Adds the generated food to the appropriate attribute*/
-        this->m_foodVector.emplace_back(tmpX,tmpY, i);
-    }
+    /*Adds the generated food to the appropriate attribute*/
+    this->m_foodVector.emplace_back(tmpX, tmpY, i);
+  }
 }
 
-
-//void GraphicSim::moveSpecimen()
+// void GraphicSim::moveSpecimen()
 //{
 
 //}
-
-
